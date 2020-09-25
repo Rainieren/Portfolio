d4546ffcec39d569dc73d6ad55f8c4ce4109362c
Rainier Laan <rainier.laan@home.nl>
2020-09-25 22:37:58 +0200

    Small changes to prevent conflict from other branch

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -263,25 +263,10 @@
                                                 <input class="appearance-none block rounded-lg w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-indigo-600" id="grid-last-name" type="text" placeholder="Example@outlook.com">
                                             </div>
                                             <div class="w-full p-3">
-                                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-                                                    Reden voor contact
-                                                </label>
-                                                <div class="relative">
-                                                    <select class="block appearance-none w-full bg-gray-300 border border-gray-200 focus:bg-gray-300 focus:border-gray-300 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none" id="grid-state">
-                                                        <option selected>Gewoon lekker babbelen</option>
-                                                        <option>Een opdrachten</option>
-                                                        <option>Anders, namelijk</option>
-                                                    </select>
-                                                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="w-full p-3">
                                                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                                                     Bericht
                                                 </label>
-                                                <textarea class="appearance-none block rounded-lg w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-indigo-600" id="grid-last-name" type="text" placeholder="Waar zit je aan te denken?"></textarea>
+                                                <textarea class="appearance-none block rounded-lg w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-indigo-600" id="grid-last-name" type="text"></textarea>
                                             </div>
                                             <div class="w-full p-3">
                                                 <button type="submit" id="" class="submit-contact-button inline-flex items-center rounded-lg px-4 py-2 border border-transparent text-base leading-6 rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150">
