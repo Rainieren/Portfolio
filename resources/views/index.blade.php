647d02bd00e42a4b6e70624233a56bebf7e52ec1
Rainier Laan <rainier.laan@home.nl>
2020-09-26 15:15:55 +0200

    Fixed overfloating CSS bug with title and language

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -139,14 +139,19 @@
                     <div class="rounded bg-gray-100 overflow-hidden shadow-lg rounded-lg flex flex-col relative">
                         <img class="w-full" src="https://techcrunch.com/wp-content/uploads/2015/04/codecode.jpg" alt="Sunset in the mountains">
                         <div class="px-6 py-4 h-full flex flex-col">
-                            <div class="project-info flex justify-between pt-1">
-                                <div class="flex w-5/6">
+
+                            <div class="project-info block min-h-8">
+                                <div class="w-5/6 float-left">
                                     <p class="font-bold gilroy text-xl">{{ $project->name }}</p>
                                 </div>
-                                <div class="flex justify-end w-1/6">
+                                <div class="flex w-1/6 float-right items-center justify-end">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5 mr-2 text-yellow-600">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                    </svg>
                                     {{ $project->stargazers_count }}
                                 </div>
                             </div>
+
                             <div class="project-language flex-1">
                                 <div class="w-full">
                                     <p class="text-gray-600 text-sm font-bold">{{ $project->language }}</p>
